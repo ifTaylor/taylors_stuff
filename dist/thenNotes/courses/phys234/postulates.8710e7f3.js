ce2034b7f8d29535a2e3e03146415f703293c027
Taylor Turner <tturner@bastiansolutions.com>
2023-04-20 16:12:18 -0500

    Main page, thenHire, thenNotes

---
+++ dist/thenNotes/courses/phys234/postulates.8710e7f3.js
@@ -0,0 +1,60 @@
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+// animate scrolling to top
+$("footer.footer").find("a").click(function() {
+    $("body").animate({
+        scrollTop: 0
+    });
+    return false;
+});
+// animate scrolling for table of contents
+$("div#contents").find("a").click(function() {
+    var $href = $(this).attr("href");
+    var $anchor = $($href).offset();
+    $("body").animate({
+        scrollTop: $anchor.top
+    });
+    return false;
+});
+// animate scrolling for sidebar
+$("ul.sidebar-nav").find("a").click(function() {
+    var $href = $(this).attr("href");
+    var $anchor = $($href).offset();
+    $("body").animate({
+        scrollTop: $anchor.top
+    });
+    return false;
+});
+// animate scrolling for other links
+$("a.link").click(function() {
+    var $href = $(this).attr("href");
+    var $anchor = $($href).offset();
+    $("body").animate({
+        scrollTop: $anchor.top
+    });
+    return false;
+});
+// update last modified date
+const lastMod = document.querySelector("#last_modified");
+lastMod.innerHTML = `Last updated on ${document.lastModified}`;
+// script for exercises to show solutions
+const exercises = document.querySelectorAll(".ex");
+exercises.forEach((ex)=>{
+    const btn = ex.querySelector("#exButton");
+    const sln = ex.querySelector("#solution");
+    btn.innerHTML = "Show solution";
+    sln.style.display = "none";
+    btn.addEventListener("click", function(e) {
+        if (sln.style.display === "none") {
+            sln.style.display = "block";
+            btn.innerHTML = "Hide solution";
+        } else {
+            sln.style.display = "none";
+            btn.innerHTML = "Show solution";
+        }
+    });
+});
+
+//# sourceMappingURL=postulates.8710e7f3.js.map
